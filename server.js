78761793e3937a6cc7dbb85f7836ca5da5c39c01
5XD9nAszYZe92ALnc_fy <sudhakar.swain@delphic.in>
2023-05-18 00:28:40 +0530

    cws

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const app = express();
 const port = 3000;
@@ -8,9 +8,9 @@ const port = 3000;
 // Database configuration
 const db = mysql.createConnection({
   host: 'localhost',
-  user: 'your_username',
-  password: 'your_password',
-  database: 'your_database',
+  user: 'root',
+  password: 'Nokia@123',
+  database: 'cws',
 });
 
 db.connect((err) => {
@@ -18,7 +18,7 @@ db.connect((err) => {
   console.log('Connected to MySQL database');
 });
 
-// Multer configuration for file upload
+// Multer configuration for file upload where i can use multiple file to upload .
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'uploads/');
@@ -29,39 +29,93 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
-
 // Sign Up API
 app.post('/signup', (req, res) => {
-  // Handle signup logic and save user to the database
-  // ...
-
-  // Send response
-  res.json({ message: 'User signed up successfully' });
-});
-
-// Login API
+    // Get user data from the request body
+    const { username, email, password } = req.body;
+  
+    // Validate user data 
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: 'Please provide all the required fields' });
+    }
+  
+    // Check if the user already exists in the database 
+    const checkUserQuery = 'SELECT * FROM users WHERE email = ?';
+    db.query(checkUserQuery, [email], (err, results) => {
+      if (err) {
+        console.error('Error checking user:', err);
+        return res.status(500).json({ message: 'An error occurred' });
+      }
+  
+      if (results.length > 0) {
+        return res.status(409).json({ message: 'User with this email already exists' });
+      }
+  
+      // Insert the new user into the database
+      const insertUserQuery = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
+      db.query(insertUserQuery, [username, email, password], (err, result) => {
+        if (err) {
+          console.error('Error inserting user:', err);
+          return res.status(500).json({ message: 'An error occurred' });
+        }
+  
+        // Send response
+        res.status(201).json({ message: 'User signed up successfully' });
+      });
+    });
+  });
+  // Login API
 app.post('/login', (req, res) => {
-  // Handle login logic and authenticate user
-  // ...
-
-  // Send response with user data
-  const user = {
-    id: 1,
-    username: 'john.doe',
-    email: 'john@example.com',
-  };
-
-  res.json({ user });
-});
+    // Get user credentials from the request body
+    const { email, password } = req.body;
+  
+    // Validate user credentials
+    if (!email || !password) {
+      return res.status(400).json({ message: 'Please provide email and password' });
+    }
+  
+    // Check if the user exists in the database and verify the credentials
+    const checkUserQuery = 'SELECT * FROM users WHERE email = ? AND password = ?';
+    db.query(checkUserQuery, [email, password], (err, results) => {
+      if (err) {
+        console.error('Error checking user:', err);
+        return res.status(500).json({ message: 'An error occurred' });
+      }
+  
+      if (results.length === 0) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+      }
+  
+      // User authenticated, send response with user data
+      const user = {
+        id: results[0].id,
+        username: results[0].username,
+        email: results[0].email,
+      };
+  
+      res.json({ user });
+    });
+  });
+  
 
 // Update Profile API with multiple file upload
 app.post('/update-profile', upload.array('images', 2), (req, res) => {
-  // Handle profile update logic
-  // ...
+    // Get user ID from the request 
+    const userId = req.userId; 
+  
+    const files = req.files;
+  
+    // Check if files were uploaded
+    if (!files || files.length === 0) {
+      return res.status(400).json({ message: 'Please upload at least one file' });
+    }
+  
+    // Process the uploaded files 
+    const fileUrls = files.map((file) => file.path);
 
-  // Send response
-  res.json({ message: 'Profile updated successfully' });
-});
+    res.json({ message: 'Profile updated successfully' });
+  });
+  
 
 // Admin Dashboard API to fetch users list
 app.get('/admin/users', (req, res) => {
